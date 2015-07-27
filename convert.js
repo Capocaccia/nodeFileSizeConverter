9e1dec7577974a638e5c699a33cbf77ef8e0f001
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-07-27 07:15:12 -0500

    refactoring

--- convert.js
+++ convert.js
@@ -1,10 +1,5 @@
 #!/usr/bin/env node
 
-
-
-// I think I want to rewrite this whole thing.
-//For example:  Set a v
-
 // bits = b
 // bytes = by
 // Kilobyte = kb
@@ -18,13 +13,13 @@ var help = require('./help/help');
 var size = process.argv.slice(2, 3);
 var format = process.argv.slice(3, 4);
 
-// if (size[0] === "table" || "Table"){
-//   console.log(" 8 bits = 1 byte \n 1024 bytes = 1 KB \n 1024 KB = 1 MB \n 1024 MB = 1 GB \n 1024 GB = 1 TB \n 1024 TB = 1 PB")
-// }
+if (size[0] === "table"){
+  console.log("\n 8 bits = 1 byte \n 1000 bytes = 1 KB \n 1000 KB = 1 MB \n 1000 MB = 1 GB \n 1000 GB = 1 TB \n 1000 TB = 1 PB")
+}
 
-// if(size[0] === 'help' || 'Help'){
-//   help()
-// }
+if(size[0] === 'help'){
+  help()
+}
 
 
 if(format[0] === "b"){
@@ -52,55 +47,60 @@ if(format[0] === "gb"){
 // }
 
 function convertB(){
-  console.log('____________________________\n' + '\nYour file sized ' + size + ' ' + 'bit(s) is: ' + '\n____________________________\n')
+  console.log('_____________________________________\n' + '\nYour file with a size of ' + size + ' ' + 'bit(s) is: \n')
   console.log(' ' + size / 8 + ' Bytes')
   console.log(' ' + size / 8000 + ' KB')
   console.log(' ' + size / 8000000 + ' MB')
   console.log(' ' + size / 8000000000 + ' GB')
   console.log(' ' + size / 8000000000000 + ' TB')
   console.log(' ' + size / 8000000000000000 + ' PB')
+  console.log('__________________________________________\n')
 }
 
 
 function convertBy(){
-  console.log('_____________________________\n' + '\nYour file sized ' + size + ' ' + 'byte(s) is: ' + '\n_____________________________\n')
+  console.log('______________________________________\n' + '\nYour file with a size of ' + size + ' ' + 'byte(s) is: \n')
   console.log(' ' + size * 8 + ' Bits')
   console.log(' ' + size / 1000 + ' KB')
   console.log(' ' + size / 1000000 + ' MB')
   console.log(' ' + size / 1000000000 + ' GB')
   console.log(' ' + size / 1000000000000 + ' TB')
   console.log(' ' + size / 1000000000000000 + ' PB')
+  console.log('__________________________________________\n')
 }
 
 
 function convertKb(){
-  console.log('_________________________________\n' + '\nYour file sized ' + size + ' ' + 'kilobyte(s) is: ' + '\n_________________________________\n')
+  console.log('__________________________________________\n' + '\nYour file with a size of ' + size + ' ' + 'kilobyte(s) is: \n')
   console.log(' ' + size * 8000 + ' Bits\n')
   console.log(' ' + size * 1000 + ' Bytes\n')
   console.log(' ' + size / 1000 + ' MB\n')
   console.log(' ' + size / 1000000 + ' GB\n')
   console.log(' ' + size / 1000000000 + ' TB\n')
   console.log(' ' + size / 1000000000000 + ' PB\n')
+  console.log('__________________________________________\n')
 }
 
 function convertMb(){
-  console.log('_________________________________\n' + '\nYour file sized ' + size + ' ' + 'megabyte(s) is: ' + '\n_________________________________\n')
+  console.log('__________________________________________\n' + '\nYour file with a size of ' + size + ' ' + 'megabyte(s) is: \n')
   console.log(' ' + size * 8000000 + ' Bits\n')
   console.log(' ' + size * 1000000 + ' Bytes\n')
   console.log(' ' + size * 1000 + ' KB\n')
   console.log(' ' + size / 1000 + ' GB\n')
   console.log(' ' + size / 1000000 + ' TB\n')
   console.log(' ' + size / 1000000000 + ' PB\n')
+  console.log('__________________________________________\n')
 }
 
 function convertGb(){
-  console.log('_________________________________\n' + '\nYour file sized ' + size + ' ' + 'gigabyte(s) is: ' + '\n_________________________________\n')
+  console.log('__________________________________________\n' + '\nYour file with a size of ' + size + ' ' + 'gigabyte(s) is:\n' )
   console.log(' ' + size * 8000000000 + ' Bits\n')
   console.log(' ' + size * 1000000000 + ' Bytes\n')
   console.log(' ' + size * 125000 + ' KB\n')
   console.log(' ' + size * 125 + ' MB\n')
   console.log(' ' + size / 1000 + ' TB\n')
   console.log(' ' + size / 1000000 + ' PB\n')
+  console.log('__________________________________________\n')
 }
 
 // function convertTb(){

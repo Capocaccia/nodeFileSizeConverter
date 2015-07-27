765609083dc2472f3027850a60c08487f79613f4
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-07-27 11:37:12 -0500

    refactored to add pentabyte calculation

--- convert.js
+++ convert.js
@@ -7,6 +7,7 @@ var convertKb = require('./lib/kb');
 var convertMb = require('./lib/mb');
 var convertGb = require('./lib/gb');
 var convertTb = require('./lib/tb');
+var convertTb = require('./lib/pb');
 var size = process.argv.slice(2, 3);
 var format = process.argv.slice(3, 4);
 
@@ -47,5 +48,9 @@ if(format[0] === "gb"){
 }
 
 if(format[0] === "tb"){
+  convertTb()
+}
+
+if(format[0] === "pb"){
   convertTb()
 }

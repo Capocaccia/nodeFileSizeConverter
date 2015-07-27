3ab434a02d1d0f64a5be5f4abf394e58dd415853
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-07-27 13:02:58 -0500

    fixed error in pentabyte conversion

--- convert.js
+++ convert.js
@@ -7,7 +7,7 @@ var convertKb = require('./lib/kb');
 var convertMb = require('./lib/mb');
 var convertGb = require('./lib/gb');
 var convertTb = require('./lib/tb');
-var convertTb = require('./lib/pb');
+var convertPb = require('./lib/pb');
 var size = process.argv.slice(2, 3);
 var format = process.argv.slice(3, 4);
 
@@ -57,5 +57,5 @@ if(format[0] === "tb"){
 }
 
 if(format[0] === "pb"){
-  convertTb()
+  convertPb()
 }

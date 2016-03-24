997203916bc8ce1a892238824caa75b7a1b7cdef
Carter Capocaccia <carter.capocaccia@gmail.com>
2016-03-24 15:43:53 -0500

    Adds semicolons

--- convert.js
+++ convert.js
@@ -17,44 +17,43 @@ switch (params[0]){
     console.log("\n 8 bits = 1 byte \n 1000 bytes = 1 KB \n 1000 KB = 1 MB \n 1000 MB = 1 GB \n 1000 GB = 1 TB \n 1000 TB = 1 PB")
     break;
   case "help":
-    help()
+    help();
     break;
 }
 
 switch (params[1]){
   case "b":
-    convertB(params)
+    convertB(params);
     break;
   case "by":
-    convertBy(params)
+    convertBy(params);
     break;
   case "kb":
-    convertKb(params)
+    convertKb(params);
     break;
   case "mb":
-    convertMb(params)
+    convertMb(params);
     break;
   case "gb":
-    convertGb(params)
+    convertGb(params);
     break;
   case "tb":
-    convertTb(params)
+    convertTb(params);
     break;
   case "pb":
-    convertPb(params)
-    break;
+    convertPb(params);
 }
 
 if(params[0] !== "table" && !params[1]){
-  console.log('\n')
-  console.log(' ' + 'Error:\n')
+  console.log('\n');
+  console.log(' ' + 'Error:\n');
   console.log(' ' + "The file format was missing or entered incorrectly.");
   console.log(' ' + "Please correct the error and try again.");
   console.log(' ' + "If you need more information, please reference the help docs by typing: node convert.js help");
 }
 
 if(!params[0]){
-  console.log('\n')
-  console.log('Error: No arguments given.  Please reference the help docs below.\n')
+  console.log('\n');
+  console.log('Error: No arguments given.  Please reference the help docs below.\n');
   help()
 }
